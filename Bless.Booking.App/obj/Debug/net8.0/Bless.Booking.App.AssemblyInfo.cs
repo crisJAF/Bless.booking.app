3a1a97c579359f96477959521abee8a68cee4c73
Cristopher Avendano <Cristopher Avendano@LAPTOP-T73LT8H9>
2025-05-09 14:46:29 -0600

    adding services section, worksday, footer

--- Bless.Booking.App/obj/Debug/net8.0/Bless.Booking.App.AssemblyInfo.cs
+++ Bless.Booking.App/obj/Debug/net8.0/Bless.Booking.App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bless.Booking.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e691c94cf7f3d1c209a58b07750973f71006df6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26bcf08fda7bfb240e59813568c0a2fd2f5bab20")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bless.Booking.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bless.Booking.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
