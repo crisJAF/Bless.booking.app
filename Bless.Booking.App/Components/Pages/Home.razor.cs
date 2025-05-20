df09c4e33cecd536f919783f0876dc82c89ab794
Cristopher Avendano <Cristopher Avendano@LAPTOP-T73LT8H9>
2025-05-20 14:33:52 -0600

    adding recaptcha

--- Bless.Booking.App/Components/Pages/Home.razor.cs
+++ Bless.Booking.App/Components/Pages/Home.razor.cs
@@ -2,5 +2,16 @@
 {
     public partial class Home
     {
+        private bool animacionActivada = false;
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if (firstRender)
+            {
+                animacionActivada = true;
+                StateHasChanged(); // fuerza un re-render para aplicar la clase
+            }
+        }
+
     }
 }
