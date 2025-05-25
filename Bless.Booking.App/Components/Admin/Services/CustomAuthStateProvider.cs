0ae45a846489dbaa2db101f57bcabda06988fbaf
douglas-i <hdouglasu@gmail.com>
2025-05-25 01:21:03 -0600

    feat: implement login functionality

---
+++ Bless.Booking.App/Components/Admin/Services/CustomAuthStateProvider.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Bless.Booking.App.Components.Admin.Services
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CustomAuthStateProvider(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var user = _httpContextAccessor.HttpContext?.User ?? new ClaimsPrincipal(new ClaimsIdentity());
+            return Task.FromResult(new AuthenticationState(user));
+        }
+
+        public void MarkUserAsLoggedOut()
+        {
+            var anonymous = new ClaimsPrincipal(new ClaimsIdentity());
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(anonymous)));
+        }
+    }
+
+}
